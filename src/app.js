9a74b49a617ccfbf02999a31ba0de70b7a8968dc
Humberto López <g.lops.h1@gmail.com>
2022-02-11 14:39:50 +0100

    Changing server port

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import { options } from "./swaggerOptions";
 const app = express();
 const specs = swaggerJSDoc(options);
 
-app.set("port", 1000);
+app.set("port", 8000);
 
 app.use(cors());
 app.use(morgan("dev"));
