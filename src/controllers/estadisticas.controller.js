868a7e75189b7c08dcea14a6a84fddea51fd6892
Humberto López <g.lops.h1@gmail.com>
2022-02-15 12:21:36 +0100

    Fix

--- src/controllers/estadisticas.controller.js
+++
@@ -1,6 +0,0 @@
-import { connect } from "../database";
-
-export const getCompositores = async (req, res) => {
-    const connection = await connect();
-    const [rows] = await connection.execute("SELECT * FROM compositores");
-}
