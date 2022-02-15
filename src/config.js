d97ca419b5e8f24703218836a6f27103748ee657
Humberto López <g.lops.h1@gmail.com>
2022-02-15 11:17:51 +0100

    Adding db port

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { config as dotenv } from "dotenv";
 dotenv();
 
 export const config = {
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST || "localhost:3306",
   user: process.env.DB_USER || "stats",
   password: process.env.DB_PASSWORD || "Password123._",
   database: process.env.DB_DATABASE || "stats",
