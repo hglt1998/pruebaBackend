fc935c99ab310deb14523f5eef55c14a9a6d344b
Humberto López <g.lops.h1@gmail.com>
2022-02-15 11:14:08 +0100

    Server config

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { config as dotenv } from "dotenv";
 dotenv();
 
 export const config = {
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST || "http://magical-zhukovsky.217-76-139-29.plesk.page",
   user: process.env.DB_USER || "stats",
   password: process.env.DB_PASSWORD || "Password123._",
   database: process.env.DB_DATABASE || "statsdb",
