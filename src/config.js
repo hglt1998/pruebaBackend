56417a57f5f512c6df1a1b23e80b9723dba09742
Humberto López <g.lops.h1@gmail.com>
2022-02-15 11:29:25 +0100

    Update

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ dotenv();
 
 export const config = {
   host: process.env.DB_HOST || "localhost:3306",
-  user: process.env.DB_USER || "stats",
+  user: process.env.DB_USER || "hglt1998",
   password: process.env.DB_PASSWORD || "Password123._",
   database: process.env.DB_DATABASE || "stats",
 };
